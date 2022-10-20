1458277440560a2179e4d171027666ffaa26f029
Benjamin Röjder Delnavaz <benjamin.rojderdelnavaz@gmail.com>
2022-10-21 01:12:24 +0200

    🚧 Current favorites progress

--- resources/views/components/snippet-item.blade.php
+++ resources/views/components/snippet-item.blade.php
@@ -1,10 +1,25 @@
 @props(['snippet'])
 
+@php
+$isFavorite = request()->user()->favorites->contains($snippet);
+@endphp
+
 <li class="rounded-lg">
-    <a href="{{ route('snippets.show', ['snippet' => $snippet->id]) }}">
-        <div class="border px-4 py-4 rounded-lg shadow-sm hover:bg-gray-50">
-            <div class="font-bold text-lg">{{ $snippet->title }}</div>
-            <x-tag-list :tags="$snippet->tags" :clickable="false"></x-tag-list>
+    <a
+        href="{{ route('snippets.show', ['snippet' => $snippet->id]) }}"
+        {!! $isFavorite ? ('title="' . __('Favorite snippet') . '"') : '' !!}
+    >
+        <div class="border px-4 py-4 rounded-lg shadow-sm hover:bg-gray-50 flex items-center">
+            <div class="flex-grow">
+                <div class="font-bold text-lg">{{ $snippet->title }}</div>
+                <x-tag-list :tags="$snippet->tags" :clickable="false"></x-tag-list>
+            </div>
+
+            @if ($isFavorite)
+                <div class="flex-shrink-0">
+                    <x-icons.star class="w-5 h-5 text-yellow-300" :title="__('Favorite snippet')" />
+                </div>
+            @endif
         </div>
     </a>
 </li>
