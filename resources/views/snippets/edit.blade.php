07743955553d6ed1cc8e287cbadfbe43fcbc32f9
Benjamin Röjder Delnavaz <benjamin.rojderdelnavaz@gmail.com>
2022-10-23 00:29:34 +0200

    💄 Implement dark mode for the edit snippet view

--- resources/views/snippets/edit.blade.php
+++ resources/views/snippets/edit.blade.php
@@ -5,9 +5,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <h2 class="font-bold text-2xl mb-4">{{ __('Edit Snippet') }}</h2>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg dark:bg-gray-800">
+                <div class="p-6 border-b border-gray-200 dark:border-gray-700">
+                    <h2 class="font-bold text-2xl mb-4 text-black dark:text-gray-200">{{ __('Edit Snippet') }}</h2>
 
                     <form method="POST" action="{{ route('snippets.update', ['snippet' => $snippet->id]) }}">
                         @method('PUT')
@@ -15,25 +15,30 @@
 
                         <div class="grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
                             <div class="sm:col-span-6">
-                                <label for="title" class="block text-sm font-medium text-gray-700">{{ __('Title') }}</label>
+                                <x-input-label for="title">{{ __('Title') }}</x-input-label>
+
                                 <div class="mt-1">
                                     <x-text-input type="text" id="title" name="title" class="w-full" :value="old('title', $snippet->title)" />
                                 </div>
+
                                 <x-input-error :messages="$errors->get('title')" class="mt-1" />
                             </div>
 
                             <div class="sm:col-span-6">
-                                <label for="tags" class="block text-sm font-medium text-gray-700">{{ __('Tags') }}</label>
+                                <x-input-label for="title">{{ __('Tags') }}</x-input-label>
+
                                 <div class="mt-1">
                                     <x-tag-select id="tags" name="tags" :value="old('tags', json_encode($snippet->tags->map->name))" />
                                 </div>
                             </div>
 
                             <div class="sm:col-span-6">
-                                <label for="content" class="block text-sm font-medium text-gray-700">{{ __('Content') }}</label>
+                                <x-input-label for="title">{{ __('Content') }}</x-input-label>
+
                                 <div class="mt-1">
                                     <x-textarea id="content" name="content" rows="12">{{ $snippet->content }}</x-textarea>
                                 </div>
+
                                 <x-input-error :messages="$errors->get('content')" class="mt-1" />
                             </div>
                         </div>
@@ -41,7 +46,7 @@
                         @include('snippets.file-form', ['defaultFiles' => json_encode($snippet->files)])
 
                         <div class="mt-6">
-                            <x-primary-button>Update</x-primary-button>
+                            <x-primary-button>{{ __('Update') }}</x-primary-button>
                         </div>
                     </form>
                 </div>
