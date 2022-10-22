b781e1ccd5bf24a0740fa10d45d31ae0b95eed49
Benjamin Röjder Delnavaz <benjamin.rojderdelnavaz@gmail.com>
2022-10-22 23:13:40 +0200

    💄 Implement dark mode for the snippet list

--- resources/views/snippets/index.blade.php
+++ resources/views/snippets/index.blade.php
@@ -5,10 +5,10 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg dark:bg-gray-800">
+                <div class="p-6 border-b border-gray-200 dark:border-gray-700">
                     <div class="flex justify-between items-center mb-4">
-                        <h2 class="font-bold text-xl">{{ $title }}</h2>
+                        <h2 class="font-bold text-xl text-black dark:text-gray-200">{{ $title }}</h2>
 
                         <form method="GET">
                             <div class="flex gap-x-4 items-center">
@@ -16,7 +16,7 @@
                                     <select
                                         id="limit"
                                         name="limit"
-                                        class="rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                        class="rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white dark:bg-gray-700 dark:text-gray-200 dark:border-gray-800"
                                     >
                                         <option @selected(request('limit') == 5)>5</option>
                                         <option @selected(request('limit', 10) == 10)>10</option>
@@ -25,7 +25,7 @@
                                         <option @selected(request('limit') == 75)>75</option>
                                         <option @selected(request('limit') == 100)>100</option>
                                     </select>
-                                    <label for="limit">{{ __('Per page') }}</label>
+                                    <label for="limit" class="text-black dark:text-gray-200">{{ __('Per page') }}</label>
                                 </div>
 
                                 <x-primary-button>{{ __('Filter') }}</x-primary-button>
