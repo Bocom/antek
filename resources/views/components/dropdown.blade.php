ffcb2577481f9deb35c6910c717246751eaee072
Benjamin Röjder Delnavaz <benjamin.rojderdelnavaz@gmail.com>
2022-10-22 15:32:33 +0200

    💄 Implement dark mode for the dashboard

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,4 +1,4 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white'])
+@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white text-b dark:bg-gray-800'])
 
 @php
 switch ($align) {
@@ -35,10 +35,10 @@ switch ($width) {
         x-transition:leave-start="transform opacity-100 scale-100"
         x-transition:leave-end="transform opacity-0 scale-95"
         class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
-        style="display: none;"
+        x-cloak
         @click="open = false"
     >
-        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
+        <div class="rounded-md ring-1 ring-black ring-opacity-5 dark:ring-gray-700 {{ $contentClasses }}">
             {{ $content }}
         </div>
     </div>
--- resources/views/components/snippet-item.blade.php
+++ resources/views/components/snippet-item.blade.php
@@ -9,9 +9,9 @@ $isFavorite = request()->user()->favorites->contains($snippet);
         href="{{ route('snippets.show', ['snippet' => $snippet->id]) }}"
         {!! $isFavorite ? ('title="' . __('Favorite snippet') . '"') : '' !!}
     >
-        <div class="border px-4 py-4 rounded-lg shadow-sm hover:bg-gray-50 flex items-center">
+        <div class="border px-4 py-4 rounded-lg shadow-sm bg-white hover:bg-gray-50 flex items-center dark:bg-gray-700 dark:border-gray-700 dark:hover:bg-gray-600">
             <div class="flex-grow">
-                <div class="font-bold text-lg">{{ $snippet->title }}</div>
+                <div class="font-bold text-lg text-black dark:text-gray-200">{{ $snippet->title }}</div>
                 <x-tag-list :tags="$snippet->tags" :clickable="false"></x-tag-list>
             </div>
 
